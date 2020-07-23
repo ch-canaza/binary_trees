01453384f4f77b070d1e90d5b2d62a148a5598b2
ch-canaza <ch-canaza@hotmail.com>
2020-07-23 13:53:14 -0500

    including more ancestors

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,16 +10,27 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 					const binary_tree_t *second)
 {
+	binary_tree_t *temp1, *temp2;
+
 	if (!first || !second)
 		return (NULL);
-	if (first == second->parent)
-		return (second->parent);
-	if (first->parent == second->parent || first->parent
-					== second->parent->parent)
-		return (first->parent);
-	if (second == first->parent)
-		return (first->parent);
-	if (second->parent == first->parent->parent)
-		return (second->parent);
+
+		if (first == second->parent)
+			return (second->parent);
+		if (second == first->parent)
+			return (first->parent);
+		temp1 = first->parent;
+		temp2 = second->parent;
+		while (first)
+		{
+			while (temp2)
+			{
+				if (temp1 == temp2)
+					return (temp1);
+			temp2 = temp2->parent;
+			}
+			temp1 = temp1->parent;
+
+		}
 	return (NULL);
 }
