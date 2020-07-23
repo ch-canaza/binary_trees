032255e91d9bc4213c761b27fa66ee8f6b992a5f
ch-canaza <ch-canaza@hotmail.com>
2020-07-23 14:08:46 -0500

    adding a condition

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -15,6 +15,8 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	if (!first || !second)
 		return (NULL);
 
+		if (first == second)
+			return ((binary_tree_t *)first);
 		if (first == second->parent)
 			return (second->parent);
 		if (second == first->parent)
