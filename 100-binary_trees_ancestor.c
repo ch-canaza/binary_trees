fb25ec590b861cec32aaf7d2b55b2f94916cdce1
ch-canaza <ch-canaza@hotmail.com>
2020-07-23 10:48:22 -0500

    first upload (find ancestors

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - finds for the lowest common ancestor of two nodes
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ * Return: pointer to the lowest ancestor/ null if no ancestor
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, 
+					const binary_tree_t *second)
+{
+	if (!first || !second)
+		return (NULL);
+	if (first == second->parent)
+		return (second->parent);
+	if (first->parent == second->parent || first->parent
+					== second->parent->parent)
+		return (first->parent);
+	if (second == first->parent)
+		return (first->parent);
+	if (second->parent == first->parent->parent)
+		return (second->parent);
+	return (NULL);
+	
+}
