8fe80ee76b0d0fe1275057136605d25b63bde583
ch-canaza <ch-canaza@hotmail.com>
2020-07-24 18:59:19 -0500

    fixing order mistake

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -14,7 +14,8 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 	tree->parent = tree->left;
 	if (tree->left->right)
 	{
-		tree->left = tree->parent->right;
+		tree->left = tree->left->right;
+		tree->left->parent = tree;
 		tree->parent->right = tree;
 		return (tree->parent);
 	}
