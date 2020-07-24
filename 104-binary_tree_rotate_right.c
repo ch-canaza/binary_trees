0d4dcb3b961ebda0442be96d3b81c064fb9da8e7
ch-canaza <ch-canaza@hotmail.com>
2020-07-24 18:37:39 -0500

    modify rotate left to become right

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_right - function that performs left rotation
+ * on a binary tree
+ * @tree: pointer to the root node of the tree to rotate
+ * Return: pointer to the new root node
+ */
+
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	if (!tree)
+		return (NULL);
+	tree->parent = tree->left;
+	if (tree->left->right)
+	{
+		tree->left = tree->parent->right;
+		tree->parent->right = tree;
+		return (tree->parent);
+	}
+	tree->left->right = tree;
+	tree->left = NULL;
+	return (tree->parent);
+
+}
